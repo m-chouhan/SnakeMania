51f10ce61201731a0cf0100037ddde0ad67a0c27
Mahendra Chouhan <mchouhanofficial@gmail.com>
2019-01-20 17:32:25 +0530

    Added Main/Options/Game menus to the scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     public void Quit() {
-
+        Debug.Log("Exiting the game");
+        Application.Quit();
     }
-
     public void PlayGame() {
-        
+        Debug.Log("Loading Game");
+    }
+    public void ToggleMusic() {
+        Debug.Log("Toggle Music");
     }
 }
