aa5acd0846bf944e249343ec5e2dcfe913912088
Mahendra Chouhan <mchouhanofficial@gmail.com>
2019-01-24 20:31:36 +0530

    fixed text out of screen bug

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,72 +5,82 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    enum GameState {
+    public enum GameState {
         NOT_STARTED,
         IN_PROGRESS,
         PAUSED,
         FINISHED
     }
-    GameState currentGameState;
+    public static GameState currentGameState;
     public const int MAX_REQUIRED = 2; 
     public Text scoreText;
     public Text winText;
     public Text restartText;
     public GameObject consumable;
-    public GameObject gamePanel; 
+    public MainMenu mainMenu; 
     public Text timerText;
     private int remainingItems;
     private float timer;
     void Start()
     {
         currentGameState = GameState.NOT_STARTED;
+        Time.timeScale = 0f;
+        GameObject menuObject = GameObject.FindWithTag("MainMenu");
+        if(menuObject != null)
+            mainMenu = menuObject.GetComponent<MainMenu>();
+        else Debug.Log("Unable to find main menu controller");
     }
-
-    void startGame() {
+    public void startGame() {
+        Debug.Log("Starting new Game!");
         currentGameState = GameState.IN_PROGRESS;
         timer = 0;
-        setRemainingItems(MAX_REQUIRED);
-        CreateConsumable();
-    }
-    void setRemainingItems(int score) {
-        remainingItems = score;
+        Time.timeScale = 1f;
+        remainingItems = MAX_REQUIRED;
         scoreText.text = "Remaining : " + remainingItems.ToString();
+        createConsumable();
+    }
+    public void endGame() {
+        Debug.Log("Game finished!");
+        currentGameState = GameState.FINISHED;
+        Time.timeScale = 0;
+        mainMenu.endGame((int)timer);
+    }
+    public void pauseGame() {
+        Debug.Log("Game is paused");
+        currentGameState = GameState.PAUSED;
+        Time.timeScale = 0f;
+    }
+    public void resumeGame() {
+        Debug.Log("Resuming game!");
+        currentGameState = GameState.IN_PROGRESS;
+        Time.timeScale = 1f;
     }
     public void updateScoreBy(int delta) {
         remainingItems -= delta;
         scoreText.text = "Remaining : "+ remainingItems.ToString();
         if(remainingItems <= 0)
-            GameOver();
+            Invoke("endGame",1f);
         else 
-            Invoke("CreateConsumable",1f);     
+            Invoke("createConsumable",1f);     
     }
+
+    //TODO : adds time penalty on every invalid collision
     public void addTimePenalty() {
         timer += 2f;
     }
-    public void CreateConsumable() {
+    public void createConsumable() {
         Instantiate(
                 consumable, 
                 new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10,10), 0), 
                 Quaternion.identity);
     }
-    public void GameOver() {
-        currentGameState = GameState.FINISHED;
-        winText.text = "Congrats!!\nYou finished the game in "+((int)timer).ToString()+" seconds";
-    }
     public int getScore() {
         return remainingItems;
     }
     void Update() {
-        switch(currentGameState) {
-            case GameState.NOT_STARTED : 
-                    //Application.LoadLevel(Application.loadedLevel);
-            break;
-            case GameState.IN_PROGRESS :
-                timer += Time.deltaTime;
-                timerText.text =  "Time " + ((int)timer).ToString();
-            break;
-            case GameState.PAUSED :
-            break;
+        if(currentGameState == GameState.IN_PROGRESS) {
+            timer += Time.deltaTime;
+            timerText.text =  "Time " + ((int)timer).ToString();
         }
     }
 }
