9554a2fe5746f0d1f9e200afb375dd7a8fbad3ea
Mahendra Chouhan <mchouhanofficial@gmail.com>
2019-02-03 15:23:22 +0530

    added space button for size increase

--- Assets/Scripts/Snake2D.cs
+++ Assets/Scripts/Snake2D.cs
@@ -10,9 +10,11 @@ public class Snake2D : MonoBehaviour
     public Joystick joystick;
     public GameObject tailPrefab;
     public float maxScaleDiff = 0.08f;
-    public float cycleCount = 4f;
+    public float curveFactor = 4f;
+    public float unitDistance = 0.05f;
+    public int startLength = 10;
     private List<Trans> historyQ;
-    private List<GameObject> tailList;
+    private List<GameObject> trailList;
     private Rigidbody2D rb;
     private TrailRenderer tail;
     private GameController gameController;
@@ -20,20 +22,23 @@ public class Snake2D : MonoBehaviour
         public Vector3 pos; //current position
         public Quaternion quat; //current rotation
         public Trans(Vector3 pos,Quaternion quat) { this.pos = pos; this.quat = quat;}
+        public Trans Clone() {
+            return new Trans(pos,quat);
+        }
     }
     void Start()
     {
         historyQ = new List<Trans>();
-        tailList = new List<GameObject>();
+        trailList = new List<GameObject>();
         rb = GetComponent<Rigidbody2D>();
         tail = GetComponent<TrailRenderer>();
         tail.time = 0.1f;
-        for(int i = 0; i < 50; ++i) {
+        for(int i = 0; i < startLength; ++i) {
             historyQ.Add(new Trans(transform.position,transform.rotation));
             GameObject tail = 
-                Instantiate(tailPrefab,transform.position,Quaternion.identity);
+                Instantiate(tailPrefab,transform.position,transform.rotation);
             tail.transform.localScale += getScaleFactorFor(i);
-            tailList.Add(tail);
+            trailList.Add(tail);
         }
 
         GameObject gameControllerObject = GameObject.FindWithTag("GameController");
@@ -48,7 +53,7 @@ public class Snake2D : MonoBehaviour
     }
 
     private Vector3 getScaleFactorFor(float index) {
-        float scaleFactor = (float) Math.Sin((Math.PI/180)*(index/50)* 180*cycleCount) * maxScaleDiff;
+        float scaleFactor = (float) Math.Sin((Math.PI/180)*index* curveFactor) * maxScaleDiff;
         return new Vector3(scaleFactor,scaleFactor,1);
     }
     // Update is called once per frame
@@ -63,13 +68,20 @@ public class Snake2D : MonoBehaviour
             transform.rotation = Quaternion.LookRotation(Vector3.forward, velocity);
 
         rb.velocity = velocity;
-        if(Vector2.Distance(tailList[49].transform.position,transform.position) > 0.1f) {
+        if(Vector2.Distance(trailList[trailList.Count-1].transform.position,transform.position) > unitDistance) {
             historyQ.Add(new Trans(transform.position,transform.rotation));
             historyQ.RemoveAt(0);
-            for(int i = 0; i < 50; ++i) {
-                tailList[i].transform.position = historyQ[i].pos;
-                tailList[i].transform.rotation = historyQ[i].quat;
+            for(int i = 0; i < trailList.Count; ++i) {
+                trailList[i].transform.position = historyQ[i].pos;
+                trailList[i].transform.rotation = historyQ[i].quat;
             }
+        }
+        if(Input.GetKeyUp("space")) {
+            GameObject trail = 
+                Instantiate(tailPrefab,historyQ[0].pos,historyQ[0].quat);
+            historyQ.Insert(0,historyQ[0].Clone());
+            trailList.Insert(0,trail);
+            trail.transform.localScale += getScaleFactorFor(trailList.Count);
         }
     }
     void OnTriggerEnter2D(Collider2D other) 
